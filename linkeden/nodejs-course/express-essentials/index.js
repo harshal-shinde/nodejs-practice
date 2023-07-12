02b99b513da4fd98f520a117eed2a0b9800639d4
h2s <harshaal.shinde@gmail.com>
2023-07-12 10:11:16 +0530

    Learned express routing fundamentals

--- linkeden/nodejs-course/express-essentials/index.js
+++ linkeden/nodejs-course/express-essentials/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 import data from './data/mock.json' assert {type : "json"}
 
 const app = express();
@@ -9,6 +9,26 @@ app.use('/images', express.static("images"));
 
 app.get('/',(req, res)=>{
   res.json(data)
+});
+
+//GET with next
+app.get('/next',(req, res, next)=>{
+  console.info('The response will be sent by the next function');
+  next();
+},(request, response)=>{
+  response.send('I just setup route with second callback')
+});
+
+//GET - Redirect method
+app.get('/redirect',(req, res)=>{
+  res.redirect('/')
+});
+
+app.get('/class/:id', (request, response)=>{
+  console.log(request.params);
+  const studentId = request.params.id;
+  const student = data.filter((student)=> student.id == studentId);
+  response.send(student)
 })
 
 app.post('/create',(req, res)=>{
@@ -23,7 +43,31 @@ app.delete('/delete',(req, res)=>{
   res.send('This is get request at /delete')
 });
 
+app.route('/class')
+.get((req, res)=>{
+  res.send('Retrieve class information');
+})
+.post((req, res)=>{
+  res.send('Create class information');
+})
+.put((req, res)=>{
+  res.send('Update class information');
+});
+
+//Route chaining
+// app.get('/class',(req, res)=>{
+//   res.send('Retrieve class information');
+// });
+
+// app.post('/class',(req, res)=>{
+//   res.send('Create class information');
+// });
+
+// app.put('/class',(req, res)=>{
+//   res.send('Update class information');
+// });
+
+
 app.listen(PORT, ()=>{
   console.info(`Nodejs server up successfully on port ${PORT}`)
-  console.log(data);
 });
